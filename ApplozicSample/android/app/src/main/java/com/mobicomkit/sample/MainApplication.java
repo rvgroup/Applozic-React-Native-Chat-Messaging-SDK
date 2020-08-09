b60a123f33ce0836257540f98a71b75f34b86636
Maycon Mesquita <mayconjcm@gmail.com>
2020-08-08 21:25:36 -0300

    Migrate android build files to be compatible with RN 0.61.x

--- ApplozicSample/android/app/src/main/java/com/mobicomkit/sample/MainApplication.java
+++ ApplozicSample/android/app/src/main/java/com/mobicomkit/sample/MainApplication.java
@@ -1,47 +1,50 @@
 package com.mobicomkit.sample;
 
 import android.app.Application;
-
+import android.content.Context;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactlibrary.ApplozicChatPackage;
-
-import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import com.reactlibrary.ApplozicChatPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-            return true;
+          return BuildConfig.DEBUG;
         }
 
         @Override
         protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
-                    new MainReactPackage(),
-                    new ApplozicChatPackage()
-            );
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          packages.add(new ApplozicChatPackage());
+          return packages;
         }
 
         @Override
         protected String getJSMainModuleName() {
-            return "index";
+          return "index";
         }
-    };
+      };
 
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
-    }
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
-    }
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
